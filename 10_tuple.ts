014cdac28e59c240307af3071bd2d4262ed9d886
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-12-02 11:15:15 +0530

    content update

---
+++ 10_tuple.ts
@@ -0,0 +1,3 @@
+// tuple - A tuple is a collection which is ordered and unchangeable.
+
+let data: [string, number, string, boolean, number] = ["asdas", 1, "dads", true, 2];
---
+++ 13_enums.ts
@@ -0,0 +1,34 @@
+// enums
+
+enum Roles{
+    SUPER_ADMIN,
+    ADMIN ,
+    MANAGER ,
+    SELLER,
+    BUYER
+
+}
+
+let myRole: Roles = Roles.ADMIN;
+
+
+function sendMessage(role:Roles){
+    switch (role){
+        case Roles.SUPER_ADMIN:
+            console.log("Hello , Super Admin");
+            break;
+        case Roles.ADMIN:
+            console.log("Hello , Admin");
+            break;
+        case Roles.MANAGER:
+            console.log("Hello , Manager");
+            break;
+        case Roles.SELLER:
+            console.log("Hello , Seller");
+            break;
+        default:
+            console.log("Hello , Buyer");
+    }
+}
+
+sendMessage(Roles.SELLER);
