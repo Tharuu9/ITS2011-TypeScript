e08e7d510c45aaf39f1c6d12343dd6b67275937c
Tharuu Wijesekara <114207360+Tharuu9@users.noreply.github.com>
2023-12-02 11:22:51 +0530

    abstract update

---
+++ 15_abstract.ts
@@ -0,0 +1,31 @@
+//abstract
+
+
+abstract class Animal{
+    abstract move(): void;
+    eat(): void{
+        console.log("Eat via mouth");
+    }
+}
+
+
+class Cat extends Animal{
+    move(): void {
+        console.log("Using legs")
+    }
+
+}
+
+class Bird extends Animal{
+    move(): void {
+        console.log("Using Feathers")
+    }
+
+}
+
+let animal1: Animal = new Cat();
+animal1.eat();
+animal1.move();
+let animal2: Animal = new Bird();
+animal2.eat();
+animal2.move();
